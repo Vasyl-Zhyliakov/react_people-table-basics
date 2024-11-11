68eb3392a6436f22686aebbe29f1b62af0773e9f
Vasyl Zhyliakov <snak87@gmail.com>
2024-11-11 17:13:38 +0200

    First solution

---
+++ src/components/PersonForTable.tsx
@@ -0,0 +1,68 @@
+import { Link, useParams } from 'react-router-dom';
+import { Person } from '../types';
+import classNames from 'classnames';
+
+type Props = {
+  person: Person;
+  people: Person[];
+};
+
+export const PersonForTable: React.FC<Props> = ({ person, people }) => {
+  const { personSlug } = useParams();
+  const selectedPerson = personSlug ? personSlug : '';
+  function findParentLink(name: string | null) {
+    if (name === null) {
+      return '-';
+    }
+    const parent = people.find(person => person.name === name);
+
+    return parent?.slug;
+  }
+
+  // "#/people/jan-van-brussel-1714"
+  return (
+    <tr
+      data-cy="person"
+      key={person.slug}
+      className={classNames({
+        'has-background-warning': selectedPerson === person.slug,
+      })}
+    >
+      <td>
+        <Link
+          to={`../${person.slug}`}
+          className={classNames({ 'has-text-danger': person.sex === 'f' })}
+        >
+          {person.name}
+        </Link>
+      </td>
+
+      <td>{person.sex}</td>
+      <td>{person.born}</td>
+      <td>{person.died}</td>
+      <td>
+        {' '}
+        {findParentLink(person.motherName) ? (
+          <Link
+            to={`../${findParentLink(person.motherName)}`}
+            className="has-text-danger"
+          >
+            {person.motherName}
+          </Link>
+        ) : (
+          person.motherName
+        )}
+      </td>
+
+      <td>
+        {findParentLink(person.fatherName) ? (
+          <Link to={`../${findParentLink(person.fatherName)}`}>
+            {person.fatherName}
+          </Link>
+        ) : (
+          person.fatherName
+        )}
+      </td>
+    </tr>
+  );
+};
